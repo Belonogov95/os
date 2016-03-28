2298e917a2211e5db929c4b5361302801ea8a761
Belonogov Ivan <Belonogov95@gmail.com>
2016-03-28 14:23:24 +0300

    hw2 done

---
+++ badlinks/badlinks.sh
@@ -0,0 +1,40 @@
+
+
+go() {
+    for s in `ls $1`
+    do
+        path=$1/$s 
+        if test -d $path 
+        then
+            go $path
+        else
+            if test -h $path
+            then
+                realPath=$(readlink $path)
+                if ! test -e $realPath
+                then
+                    #echo slink not exist $path
+                    tmr=$(stat --format "%Y" $path)
+                    curTime=$(date +"%s")
+                    diff=$((curTime - tmr))
+                    oneWeek=$((60*60*24*7))
+
+                    if [ "$oneWeek" -lt "$diff" ]
+                    then
+                        echo $path
+                    fi
+                fi
+            fi 
+        fi
+    done
+}
+
+
+
+go $1
+
+
+
+
+
+
